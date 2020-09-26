5ed8ce62e45ac25a1e1aeaf2fc8a32d0790a4d60
MrJaroo <mishadolininma@mail.ru>
2020-09-27 00:17:18 +0400

    added pages

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,29 @@ $(function(){
         dots:true,
         arrows:false
     });
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        grid: false,
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function(){
+        $('.product-page__content .product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+        
+    });
+
+    $('.icon-th-large').on('click', function(){
+        $('.product-page__content .product__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+    });
     
 
     var mixer = mixitup('.products__inner-box');
